172f42bbe97ced58ad4c170c32670d43b3bf9fed
Vu Nguyen <nguyenleavu@gmail.com>
2022-11-07 20:56:32 +0700

    new

--- components/Product.tsx
+++ components/Product.tsx
@@ -21,8 +21,8 @@ const Product = ({ data }: Props) => {
                 src={data.image}
                 alt=''
                 className='product md:rounded-none lg:rounded-full'
-                width='600'
-                height='600'
+                width='300'
+                height='300'
                 unoptimized={true}
             />
             <div className='h-30 lg:h-40 bg-white dark:bg-[#151515] text-[#252525] dark:text-[#ddd] rounded-none'>
